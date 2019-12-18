d649b5bcfba3e7f75fc0c0cc77078067a1ab39c1
Jose Santos <jsantos108@gmail.com>
2019-12-18 12:46:16 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -43,8 +43,13 @@ class CommentForm extends React.Component {
   };
 
   handleSubmit(values) {
-    console.log("Current state is: " + JSON.stringify(values));
-    alert("Current state is: " + JSON.stringify(values));
+    this.toggleModal();
+    this.props.addComment(
+      this.props.campsiteId,
+      values.rating,
+      values.author,
+      values.text
+    );
   }
 
   render() {
@@ -132,7 +137,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1" style={{ textAlign: "left" }}>
@@ -151,7 +156,7 @@ function RenderComments({ comments }) {
             </p>
           );
         })}
-        <CommentForm />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   }
@@ -175,7 +180,11 @@ function CampsiteInfo(props) {
       </div>
       <div className="row">
         <RenderCampsite campsite={props.campsite} />
-        <RenderComments comments={props.comments} />
+        <RenderComments
+          comments={props.comments}
+          addComment={props.addComment}
+          campsiteId={props.campsite.id}
+        />
       </div>
     </div>
   ) : (
